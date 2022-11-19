88c2a9f462cf33d11501091fb07c74b838561096
Atanu <atanukundu1991@gmail.com>
2022-11-19 20:47:25 +0530

    setup.py

--- template.py
+++ template.py
@@ -17,13 +17,14 @@ logging.info(f"creating project by name: {project_name}")
 list_of_files = [
     ".github/workflows/.gitkeep",
     ".github/workflows/ci.yaml",
-    f"scr/{project_name}/__init__.py",
+    f"src/{project_name}/__init__.py",
     f"test/__init__.py",
     f"test/unit/__init__.py",
     f"configs/config.yaml",
     f"docs/index.md",
     "init_setup.sh",
     "requirements.txt",
+    "requirements_dev.txt",
     "setup.py",
     "setup.cfg",
     "tox.ini",
@@ -35,6 +36,7 @@ list_of_files = [
 for filepath in list_of_files:
     filepath = Path(filepath)
     filedir, filename = os.path.split(filepath)
+
     if filedir != "":
         os.makedirs(filedir,exist_ok=True)
         logging.info(f"creating a new directory at: {filedir} for file: {filename}")
